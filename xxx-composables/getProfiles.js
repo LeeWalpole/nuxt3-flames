5a7f84ff8b05772949bb677477721bbc5941c03f
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-01-20 16:14:50 +0700

    Update getProfiles

--- xxx-composables/getProfiles.js
+++ xxx-composables/getProfiles.js
@@ -1,16 +1,16 @@
 
 
 // getProfiles.js
-export const getProfiles = () => {
-    const getProfiles = this;
-     fetch("http://localhost:10009/wp-json/data/v1/profiles/")
-       .then((response) => {
-         return  response.json();
-       })
-       .then((res) => {
-        getProfiles.profiles = JSON.parse(JSON.stringify(res));
-           return getProfiles.profiles;
-       });
-       return useState('profiles', () => profiles)
- }
+// export const getProfiles = () => {
+//     const getProfiles = this;
+//      fetch("http://localhost:10009/wp-json/data/v1/profiles/")
+//        .then((response) => {
+//          return  response.json();
+//        })
+//        .then((res) => {
+//         getProfiles.profiles = JSON.parse(JSON.stringify(res));
+//            return getProfiles.profiles;
+//        });
+//        return useState('profiles', () => profiles)
+//  }
 
