97d26afb4b64ce1323eded15becdc18db81cc683
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-01-21 03:06:06 +0700

    Added PWA
    Followed this
    https://pwa.nuxtjs.org/setup

---
+++ nuxt.config.js
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig(
+    
+    {
+        buildModules: [
+          '@nuxtjs/pwa',
+        ]
+      },
+    {
+    
+    css: [
+    'assets/css/all.css'
+    ],
+
+}
+)
+
+
